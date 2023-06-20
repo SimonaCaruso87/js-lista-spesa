d52d39e78b07bf63d060655a3184c1226006be30
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-06-20 08:57:59 +0200

    Inizializzazione esercizio e creazione array

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-# js-lista-spesa
+/* CONSEGNA
 
-Ciao ragazze e ciao ragazzi!
-Come annunciato oggi ci sarà un recap nel pomeriggio, i dettagli saranno meglio specificati dagli insegnanti del pomeriggio più tardi, perciò l'esercizio sarà piccino.
-Esercizio di oggi: Lista della spesa con ciclo while
-nome repo: js-lista-spesa
-Consegna:
 Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 Consigli:
 - Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
 - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
-Buon lavoro a tutte e a tutti!!
+
+1)creare un array con una lista della spesa*/
+
+const listaSpesa = [
+    'avocado',
+    'uova',
+    'tonno',
+    'pancarrè',
+    'latte',
+    'basilico',
+    'pinoli',
+    'olio',
+    'parmigiano reggiano',
+];
+ console.log(listaSpesa , 'Lista Della Spesa' , typeof listaSpesa);
